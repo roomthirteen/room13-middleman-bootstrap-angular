42a043915cdc3ed6250c241066babd2875b6ca08
Benjamin Seibert <benjamin.seibert@roomthirteen.de>
2014-12-20 17:02:31 +0100

    deactivated layout for all component templates as they are loaded by angular

--- config.rb
+++ config.rb
@@ -1,59 +1,26 @@
-###
-# Compass
-###
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+# remove layout for all components templates as they are loaded with angular
+with_layout :plain do
+  page "/components/*"
+end
 
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
 activate :livereload
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+
 
 set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'img'
 
+# append bower directory to sprockets
 after_configuration do
   @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
   sprockets.append_path File.join "#{root}", @bower_config["directory"]
 end
 
+# append angular components directory to sprockets
 sprockets.append_path File.join root, "source/components"
 
 
